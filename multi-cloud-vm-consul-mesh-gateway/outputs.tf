e7a6b6e30a5407998afdb37d469bf51b03bdc414
Christoph Puhl <cpu@hashicorp.com>
2019-10-15 15:58:03 +0200

    Update outputs.tf

--- multi-cloud-vm-consul-mesh-gateway/outputs.tf
+++ multi-cloud-vm-consul-mesh-gateway/outputs.tf
@@ -30,12 +30,20 @@ output "aws_consul_client_2_public_ip" {
   value = module.aws_consul_client_2.public_ip
 }
 
+output "aws_consul_client_3_public_ip" {
+  value = module.aws_consul_client_3.public_ip
+}
+
 output "gcp_consul_client_1_public_ip" {
   value = module.gcp_consul_client_1.public_ip
 }
 
 output "gcp_consul_client_2_public_ip" {
   value = module.gcp_consul_client_2.public_ip
+}
+
+output "gcp_consul_client_3_public_ip" {
+  value = module.gcp_consul_client_3.public_ip
 }
 
 output "consul_ui_gcp" {
@@ -52,4 +60,28 @@ output "counting_dashboard_gcp" {
 
 output "counting_dashboard_aws" {
   value = "http://${module.aws_consul_client_1.public_ip}:9002"
+}
+
+output "gcp_L7_route_default" {
+  value = "http://${module.gcp_consul_client_3.public_ip}"
+}
+
+output "gcp_L7_route_admin" {
+  value = "http://${module.gcp_consul_client_3.public_ip}/admin"
+}
+
+output "gcp_L7_route_login" {
+  value = "http://${module.gcp_consul_client_3.public_ip}/login"
+}
+
+output "aws_L7_route_default" {
+  value = "http://${module.aws_consul_client_3.public_ip}"
+}
+
+output "aws_L7_route_admin" {
+  value = "http://${module.aws_consul_client_3.public_ip}/admin"
+}
+
+output "aws_L7_route_login" {
+  value = "http://${module.aws_consul_client_3.public_ip}/login"
 }
